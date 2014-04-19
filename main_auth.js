1a7ac8aa95497208e7945edbb08709372cdf34ee
weustace <weustace@eustacesheen.me.uk>
2014-04-19 23:43:57 +0100

    Almost finished initial auth code, and began work on secondary control.

--- main_auth.js
+++ main_auth.js
@@ -1,17 +1,33 @@
+var nonce = ""; //bad practice to store cryto stuff in global jscript vars...but this is only a proof of concept!
+var UID = -1;
 
 function sendInitialLoginTransaction(){
 	var agent_url = $("#agent_url_field").val();
 	var entered_username = $("#username_field").val();
 	var entered_password = $("#password_field").val();
-	var query_string = "newClient=true&username=" + entered_username + "&password=" + entered_password;
+	var query_object = { newClient:"true", username:entered_username, password:entered_password };
 	$.ajax(
 	agent_url,//agent url
 	//settings
 	{
-	data:query_string,
+	data:JSON.stringify(query_object),
 	type:'POST',
-	traditional:'true'
+	statusCode:
+		{
+		401:function(){alert("Bad username or password.");},
+		
+		},
+	complete:handleInitialLoginResponse
 	}
 	);
 }
-function handleInitialLoginResponse(data){}
+function handleInitialLoginResponse(event,req){
+		var respObj = JSON.parse(event.responseText);
+		UID = respObj[0];
+		nonce = respObj[1];//store our response object stuff in global vars
+		
+		//clear the user interface
+		$("#auth_block").slideUp("slow",function(){$("#auth_block").html("");});
+		//draw a new pane.
+		
+	}
