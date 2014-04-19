3799dc4d7f3e691d40d5c70c333556a5fc2c5264
William Eustace <webmaster@wholikesknowledge.co.uk>
2014-04-19 21:53:24 +0100

    Fixed password field and rewrote post algo.

--- main_auth.js
+++ main_auth.js
@@ -3,6 +3,15 @@ function sendInitialLoginTransaction(){
 	var agent_url = $("#agent_url_field").val();
 	var entered_username = $("#username_field").val();
 	var entered_password = $("#password_field").val();
-	$.post(agent_url, newClient=true&username=entered_username&password=entered_password,handleInitialLoginResponse(data),"json");
+	var query_string = "newClient=true&username=" + entered_username + "&password=" + entered_password;
+	$.ajax(
+	agent_url,//agent url
+	//settings
+	{
+	data:query_string,
+	type:'POST',
+	traditional:'true'
+	}
+	);
 }
 function handleInitialLoginResponse(data){}
